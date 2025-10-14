31a087e814aa6b46ab4a6220fa56ae33f66c098a
Emmanuel-Guerreiro <emmanuelguerreiro98@gmail.com>
2025-10-14 13:19:27 -0300

    type

--- src/api/maximos-dias-adelante-reserva/maximos-dias-adelante-reserva.type.ts
+++ src/api/maximos-dias-adelante-reserva/maximos-dias-adelante-reserva.type.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
 
 export const createMaximosDiasAdelanteReservaSchema = z.object({
-  valor: z.coerce.number().int().positive(),
+  valor: z.coerce
+    .number({
+      message: "Debe ser un valor numérico",
+    })
+    .int("Debe ser un número entero")
+    .positive("El parámetro debe ser mayor a 0"),
 });
 
 export type MaximosDiasAdelanteReservaRequest = z.infer<
